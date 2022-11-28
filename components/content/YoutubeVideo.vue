013e6397d18d986f2efa41ad89deae80d93fa315
daguitosama <tocarralero@gmail.com>
2022-11-27 22:04:47 -0500

    first post draft

---
+++ components/content/YoutubeVideo.vue
@@ -0,0 +1,23 @@
+<script setup>
+const props = defineProps({
+    videoId: {
+        type: String,
+    },
+});
+</script>
+
+<template>
+    <div class="flex items-center justify-center h-[350px] w-full p-2">
+        <div class="w-full h-full max-w-2xl mx-auto">
+            <iframe
+                height="100%"
+                width="100%"
+                :src="'https://www.youtube.com/embed/' + videoId"
+                frameborder="0"
+                allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+                class="rounded-xl"
+            ></iframe>
+        </div>
+    </div>
+</template>
